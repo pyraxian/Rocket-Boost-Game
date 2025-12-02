64646f6d8bc4ede7ea896ca8986925164f7a91b6
Connor Neill <33159621+pyraxian@users.noreply.github.com>
2025-12-02 12:47:10 -0500

    Oscillation script setup
    Created primary variables for the oscillation script.
    
    Placed three objects in level to apply script to.

--- Assets/Scripts/Oscillation.cs
+++ Assets/Scripts/Oscillation.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Oscillation : MonoBehaviour
 {
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private Vector3 movementVector;
+    private float speed;
+    private float movementFactor;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
