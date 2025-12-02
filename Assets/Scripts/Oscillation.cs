b0ac728b1529d95c306c8d0901715d196289f8d8
Connor Neill <33159621+pyraxian@users.noreply.github.com>
2025-12-02 14:48:56 -0500

    Add movement function to Oscillation
    Added functionality to Oscillation by way of Vector3.Lerp and Mathf.PingPong.
    
    Added more scenery elements to second level.

--- Assets/Scripts/Oscillation.cs
+++ Assets/Scripts/Oscillation.cs
@@ -2,21 +2,23 @@ using UnityEngine;
 
 public class Oscillation : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 movementVector;
     private Vector3 startPosition;
     private Vector3 endPosition;
-    private Vector3 movementVector;
-    private float speed;
     private float movementFactor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startPosition = transform.position;
+        endPosition = startPosition + movementVector;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementFactor = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(startPosition, endPosition, movementFactor);
     }
 }
